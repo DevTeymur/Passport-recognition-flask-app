98b3c18891c9f0f83683b68a74b521ae23009e95
Teymur <teymurr.601@gmail.com>
2022-06-14 16:49:19 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from crypt import methods
+from unittest import result
+from urllib import response
+from flask import Flask, render_template
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+
+UPLOAD_FOLDER = 'static/uploads/'
+ 
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+     
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def f(filename):
+    return  filename + ' back'
+
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('Image successfully uploaded')
+        return render_template('index.html', c=f(filename))
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+    
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
