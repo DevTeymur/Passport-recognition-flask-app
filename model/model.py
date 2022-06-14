7251a60154041b24601d1a435315938793c3fc37
Teymur <teymurr.601@gmail.com>
2022-06-14 23:16:00 +0400

    Final maybe

--- model/model.py
+++ model/model.py
@@ -1,23 +1,21 @@
+import tensorflow
+import os
 import keras
 import numpy as np
-from keras.preprocessing import image
-from os import listdir
-from os.path import isfile, join
-import pandas as pd
-from sklearn.metrics import classification_report
-import sys
-sys.path.append("./")
 from warnings import filterwarnings
 filterwarnings("ignore")
-import os
+
 
 # mypath="C:/Users/UFAZ/Desktop/work/Germancy/model" # path
-mypath="/mnt/c/Users/UFAZ/Desktop/work/Germancy/final/Passport-recognition-flask-app/"
-model1 = keras.models.load_model(mypath+'model/my_model.h5')
+# mypath="/mnt/c/Users/UFAZ/Desktop/work/Germancy/final/Passport-recognition-flask-app/"
+mypath = '/'
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+print(ROOT_DIR)
+model1 = keras.models.load_model(ROOT_DIR+'/my_model.h5')
 
 def returnArray(imgPath, size = 200):
 
-    img = keras.utils.load_img(mypath+"static/uploads/"+imgPath, target_size=(size, size), grayscale=False)
+    img = keras.utils.load_img(ROOT_DIR+"/../static/uploads/"+imgPath, target_size=(size, size), grayscale=False)
     # load_img('path_to_image', target_size=(img_size, img_size))
     resized_img = keras.utils.img_to_array(img)
     return np.array(resized_img).reshape(1, size, size, 3)
